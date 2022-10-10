8cfc22dba5f6239da74984aef4216ace21bf082c
Ciro Ponzi <ciroponzi@icloud.com>
2022-10-10 19:25:27 -0300

    Submit exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let addString = '';
+    for (; num > 0; num--) {
+        addString += string;
+    };
+    if (num < 0) {
+        addString = 'ERROR';
+    };
+    return addString;
 };
 
 // Do not edit below this line

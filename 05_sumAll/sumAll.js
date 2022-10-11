8abdb05070ddbcbd3f4b7e039df9cf5809b518ae
Ciro Ponzi <ciroponzi@icloud.com>
2022-10-11 16:10:26 -0300

    Submit exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let total = 0;
+    if (typeof a !== 'string' && typeof b !== 'string' && a < b && a >= 0) {
+        for (let i = a; i <= b; i++) {
+            total = total + i;
+        };
+    } else if (typeof b !== 'string' && typeof a !== 'string' && a > b && b >= 0) {
+        for (let i = b; i <= a; i++) {
+            total = total + i;
+        };
+    } else {
+        total.toString();
+        total = "ERROR";
+    }
+    return total;
 };
 
 // Do not edit below this line

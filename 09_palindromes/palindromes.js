730e1577025df9d6df533a8a9bb7f2002d9c751a
Ciro Ponzi <ciroponzi@icloud.com>
2022-10-11 19:51:47 -0300

    Submit exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
-};
+const palindromes = function (a) {
+    let confirm;
+    let after = a.toLowerCase().replace(/[^a-z]/g, "");
+    let reversed = after.split("").reverse().join("");
+    if (after === reversed) {
+        confirm = true;
+    } else {
+        confirm = false;
+    }
+    return confirm;
+}
 
 // Do not edit below this line
 module.exports = palindromes;

ea2507999b9f2c0b3b74174d80466e4465e2fd52
Ciro Ponzi <ciroponzi@icloud.com>
2022-10-11 18:16:35 -0300

    Submit exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(a) {
+  return parseFloat((((a - 32) * 5) / 9).toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(b) {
+  return parseFloat((b * 1.8 + 32).toFixed(1));
 };
 
 // Do not edit below this line

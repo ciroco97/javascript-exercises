261df4a18f64c8de603c50209716e65e6e80e4b2
Ciro Ponzi <ciroponzi@icloud.com>
2022-10-11 15:40:10 -0300

    Submit exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function([a, b, c, d], e, f, g, h) {
+    let array = [a, b, c, d];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === e || array[i] === f || array [i] === g || array[i] === h) {
+            array.splice(i, 1);
+            i--;
+        };
+    };
+    return array;
 };
 
 // Do not edit below this line

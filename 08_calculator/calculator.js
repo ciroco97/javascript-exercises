56601128426583fca360db0203e999f80008b133
Ciro Ponzi <ciroponzi@icloud.com>
2022-10-11 19:00:25 -0300

    Submit exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,54 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function([a, b, c, d, e]) {
+	let suma = 0;
+  let array = [a, b, c, d, e];
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] !== "number") {
+      suma += 0;
+    } else {
+      suma += array[i];
+    }
+  }
+  return suma;
 };
 
-const multiply = function() {
-
+const multiply = function([a, s, d, f, g, h, j]) {
+  let suma = 1;
+  let array = [a, s, d, f, g, h, j];
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] !== "number") {
+      suma += 0;
+    } else {
+      suma *= array[i];
+    }
+  }
+  return suma;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let final = a;
+	for (let i = 1; i < b; i++) {
+    final = final * a;
+  }
+  return final;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let suma = 1;
+  for (i = 1; i <= a; i++) {
+    suma = suma * i;
+  }
+  if (suma === 0) {
+    suma = 1;
+  }
+  return suma;
 };
 
 // Do not edit below this line

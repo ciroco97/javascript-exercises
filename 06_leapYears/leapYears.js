8e8785bec0bdab01c3ba1000c8161d247bcca69b
Ciro Ponzi <ciroponzi@icloud.com>
2022-10-11 17:51:38 -0300

    Submit exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let confirm;
+    if (year % 400 === 0) {
+        confirm = true;
+    } else if (year % 100 === 0) {
+            confirm = false;
+        } else if (year % 4 === 0) {
+            confirm = true;
+        } else {
+            confirm = false;
+        }
+    return confirm;
 };
 
 // Do not edit below this line

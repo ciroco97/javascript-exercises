5435e04fb70a28d8a31ba136cce04f375eca62a9
Ciro Ponzi <ciroponzi@icloud.com>
2022-10-12 19:13:19 -0300

    Submit exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,27 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let numero = parseInt(num);
+    let num1 = 1;
+    let num2 = 1;
+    if (numero < 0) {
+        return "OOPS";
+    } else {
+        for (let i = 2; i < numero; i++) {
+            if (num1 > num2) {
+                num2 = num1 + num2;
+            } else if (num1 < num2) {
+                num1 = num1 + num2;
+            } else {
+                num2 = num1 + num2;
+            }
+        }
+        if (num1 < num2) {
+            return num2;
+        } else {
+            return num1;
+        }
+}
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+

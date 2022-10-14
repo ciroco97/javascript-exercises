ff53500bac3ae9d1aa9b0f154d537ed7a8fa5bc9
Ciro Ponzi <ciroponzi@icloud.com>
2022-10-14 11:43:46 -0300

    Submit exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let current = 0;
+    let oldest = 0;
+    let name;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].yearOfDeath === undefined) {
+            array[i].yearOfDeath = new Date().getFullYear();
+        }
+        current = array[i].yearOfDeath - array[i].yearOfBirth;
+        if (current > oldest) {
+            oldest = current;
+            name = array[i];
+        };
+    }
+    return name;
 };
 
 // Do not edit below this line
